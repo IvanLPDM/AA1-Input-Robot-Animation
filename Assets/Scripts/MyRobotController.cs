994f3b9326093efe821b26fa5bb3c0369eb45a3e
Iván <ivan.leris@enti.cat>
2024-11-14 19:11:33 +0100

    MovementInput + Grab

--- Assets/Scripts/MyRobotController.cs
+++ Assets/Scripts/MyRobotController.cs
@@ -25,6 +25,15 @@ public class MyRobotController : MonoBehaviour
     private float l2;
     private float l3;
 
+    public float angle1_x = 0f;
+    public float angle1_y = 0f;
+    public float angle2 = 0f;
+    public float angle3 = 0f;
+
+    public float rotationSpeed = 50f;
+
+    private bool picked_Target = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -53,8 +62,53 @@ public class MyRobotController : MonoBehaviour
 
     void Update()
     {
+        InputHope();
         UpdateVisualLinks();
+
+        if(picked_Target)
+        {
+            catchTarget(); 
+        }
     }
+
+    void InputHope()
+    {
+        // Captura de inputs para rotar los joints
+        if (Input.GetKey(KeyCode.Q)) angle1_x += rotationSpeed * Time.deltaTime; // Rotación Joint1
+        if (Input.GetKey(KeyCode.A)) angle1_x -= rotationSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W)) angle1_y += rotationSpeed * Time.deltaTime; 
+        if (Input.GetKey(KeyCode.S)) angle1_y -= rotationSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.E)) angle2 += rotationSpeed * Time.deltaTime; // Rotación Joint2
+        if (Input.GetKey(KeyCode.D)) angle2 -= rotationSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.R)) angle3 += rotationSpeed * Time.deltaTime; // Rotación Joint3
+        if (Input.GetKey(KeyCode.F)) angle3 -= rotationSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space)) picked_Target = true;
+
+        // Calcular la posición de cada joint utilizando FK
+        CalculateFK();
+    }
+
+    void CalculateFK()
+    {
+        Vector3 position0 = Joint0.position;
+        
+        Quaternion rotation1 = Quaternion.Euler(0, angle1_y, angle1_x);
+        Vector3 position1 = position0 + rotation1 * Vector3.right * l1;
+        Joint1.position = position1;
+
+        Quaternion rotation2 = rotation1 * Quaternion.Euler(0, angle2, 0);
+        Vector3 position2 = position1 + rotation2 * Vector3.right * l2;
+        Joint2.position = position2;
+
+        Quaternion rotation3 = rotation2 * Quaternion.Euler(0, angle3, 0);
+        Vector3 positionEndEffector = position2 + rotation3 * Vector3.right * l3;
+        endFactor.position = positionEndEffector;
+
+        Joint1.rotation = rotation1;
+        Joint2.rotation = rotation2;
+        endFactor.rotation = rotation3;
+    }
+
 
     void MoveTarget(Transform target)
     {
@@ -71,5 +125,10 @@ public class MyRobotController : MonoBehaviour
 
         lineRenderer3.SetPosition(0, Joint2.position);
         lineRenderer3.SetPosition(1, endFactor.position);
+    }
+
+    void catchTarget()
+    {
+        Stud_target.position = endFactor.position;
     }
 }
